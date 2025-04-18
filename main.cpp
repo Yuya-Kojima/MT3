71d2e9dbc7df0c23e9d3eefb1cd49c0af3e73053
Yuya-Kojima <k024g0104@g.neec.ac.jp>
2025-04-18 11:18:06 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,52 +1,259 @@
 #include <Novice.h>
+#include <cmath>
 
 const char kWindowTitle[] = "LC2B_09_コジマユウヤ";
+
+struct Vector3 {
+  float x;
+  float y;
+  float z;
+};
+
+struct Matrix4x4 {
+  float m[4][4];
+};
+
+// 行列の積
+Matrix4x4 Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2) {
+
+  Matrix4x4 result;
+
+  result.m[0][0] =
+      matrix1.m[0][0] * matrix2.m[0][0] + matrix1.m[0][1] * matrix2.m[1][0] +
+      matrix1.m[0][2] * matrix2.m[2][0] + matrix1.m[0][3] * matrix2.m[3][0];
+
+  result.m[0][1] =
+      matrix1.m[0][0] * matrix2.m[0][1] + matrix1.m[0][1] * matrix2.m[1][1] +
+      matrix1.m[0][2] * matrix2.m[2][1] + matrix1.m[0][3] * matrix2.m[3][1];
+
+  result.m[0][2] =
+      matrix1.m[0][0] * matrix2.m[0][2] + matrix1.m[0][1] * matrix2.m[1][2] +
+      matrix1.m[0][2] * matrix2.m[2][2] + matrix1.m[0][3] * matrix2.m[3][2];
+
+  result.m[0][3] =
+      matrix1.m[0][0] * matrix2.m[0][3] + matrix1.m[0][1] * matrix2.m[1][3] +
+      matrix1.m[0][2] * matrix2.m[2][3] + matrix1.m[3][0] * matrix2.m[3][3];
+
+  result.m[1][0] =
+      matrix1.m[1][0] * matrix2.m[0][0] + matrix1.m[1][1] * matrix2.m[1][0] +
+      matrix1.m[1][2] * matrix2.m[2][0] + matrix1.m[1][3] * matrix2.m[3][0];
+
+  result.m[1][1] =
+      matrix1.m[1][0] * matrix2.m[0][1] + matrix1.m[1][1] * matrix2.m[1][1] +
+      matrix1.m[1][2] * matrix2.m[2][1] + matrix1.m[1][3] * matrix2.m[3][1];
+
+  result.m[1][2] =
+      matrix1.m[1][0] * matrix2.m[0][2] + matrix1.m[1][1] * matrix2.m[1][2] +
+      matrix1.m[1][2] * matrix2.m[2][2] + matrix1.m[1][3] * matrix2.m[3][2];
+
+  result.m[1][3] =
+      matrix1.m[1][0] * matrix2.m[0][3] + matrix1.m[1][1] * matrix2.m[1][3] +
+      matrix1.m[1][2] * matrix2.m[2][3] + matrix1.m[1][3] * matrix2.m[3][3];
+
+  result.m[2][0] =
+      matrix1.m[2][0] * matrix2.m[0][0] + matrix1.m[2][1] * matrix2.m[1][0] +
+      matrix1.m[2][2] * matrix2.m[2][0] + matrix1.m[2][3] * matrix2.m[3][0];
+
+  result.m[2][1] =
+      matrix1.m[2][0] * matrix2.m[0][1] + matrix1.m[2][1] * matrix2.m[1][1] +
+      matrix1.m[2][2] * matrix2.m[2][1] + matrix1.m[2][3] * matrix2.m[3][1];
+
+  result.m[2][2] =
+      matrix1.m[2][0] * matrix2.m[0][2] + matrix1.m[2][1] * matrix2.m[1][2] +
+      matrix1.m[2][2] * matrix2.m[2][2] + matrix1.m[2][3] * matrix2.m[3][2];
+
+  result.m[2][3] =
+      matrix1.m[2][0] * matrix2.m[0][3] + matrix1.m[2][1] * matrix2.m[1][3] +
+      matrix1.m[2][2] * matrix2.m[2][3] + matrix1.m[2][3] * matrix2.m[3][3];
+
+  result.m[3][0] =
+      matrix1.m[3][0] * matrix2.m[0][0] + matrix1.m[3][1] * matrix2.m[1][0] +
+      matrix1.m[3][2] * matrix2.m[2][0] + matrix1.m[3][3] * matrix2.m[3][0];
+
+  result.m[3][1] =
+      matrix1.m[3][0] * matrix2.m[0][1] + matrix1.m[3][1] * matrix2.m[1][1] +
+      matrix1.m[3][2] * matrix2.m[2][1] + matrix1.m[3][3] * matrix2.m[3][1];
+
+  result.m[3][2] =
+      matrix1.m[3][0] * matrix2.m[0][2] + matrix1.m[3][1] * matrix2.m[1][2] +
+      matrix1.m[3][2] * matrix2.m[2][2] + matrix1.m[3][3] * matrix2.m[3][2];
+
+  result.m[3][3] =
+      matrix1.m[3][0] * matrix2.m[0][3] + matrix1.m[3][1] * matrix2.m[1][3] +
+      matrix1.m[3][2] * matrix2.m[2][3] + matrix1.m[3][3] * matrix2.m[3][3];
+
+  return result;
+}
+
+// アフィン行列作成
+Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3 &rotate,
+                           const Vector3 &translate) {
+
+  Matrix4x4 scaleMatrix;
+
+  scaleMatrix.m[0][0] = scale.x;
+  scaleMatrix.m[0][1] = 0.0f;
+  scaleMatrix.m[0][2] = 0.0f;
+  scaleMatrix.m[0][3] = 0.0f;
+  scaleMatrix.m[1][0] = 0.0f;
+  scaleMatrix.m[1][1] = scale.y;
+  scaleMatrix.m[1][2] = 0.0f;
+  scaleMatrix.m[1][3] = 0.0f;
+  scaleMatrix.m[2][0] = 0.0f;
+  scaleMatrix.m[2][1] = 0.0f;
+  scaleMatrix.m[2][2] = scale.z;
+  scaleMatrix.m[2][3] = 0.0f;
+  scaleMatrix.m[3][0] = 0.0f;
+  scaleMatrix.m[3][1] = 0.0f;
+  scaleMatrix.m[3][2] = 0.0f;
+  scaleMatrix.m[3][3] = 1.0f;
+
+  Matrix4x4 rotateX;
+  rotateX.m[0][0] = 1.0f;
+  rotateX.m[0][1] = 0.0f;
+  rotateX.m[0][2] = 0.0f;
+  rotateX.m[0][3] = 0.0f;
+  rotateX.m[1][0] = 0.0f;
+  rotateX.m[1][1] = std::cos(rotate.x);
+  rotateX.m[1][2] = std::sin(rotate.x);
+  rotateX.m[1][3] = 0.0f;
+  rotateX.m[2][0] = 0.0f;
+  rotateX.m[2][1] = -std::sin(rotate.x);
+  rotateX.m[2][2] = std::cos(rotate.x);
+  rotateX.m[2][3] = 0.0f;
+  rotateX.m[3][0] = 0.0f;
+  rotateX.m[3][1] = 0.0f;
+  rotateX.m[3][2] = 0.0f;
+  rotateX.m[3][3] = 1.0f;
+
+  Matrix4x4 rotateY;
+  rotateY.m[0][0] = std::cos(rotate.y);
+  rotateY.m[0][1] = 0.0f;
+  rotateY.m[0][2] = -std::sin(rotate.y);
+  rotateY.m[0][3] = 0.0f;
+  rotateY.m[1][0] = 0.0f;
+  rotateY.m[1][1] = 1.0f;
+  rotateY.m[1][2] = 0.0f;
+  rotateY.m[1][3] = 0.0f;
+  rotateY.m[2][0] = std::sin(rotate.y);
+  rotateY.m[2][1] = 0.0f;
+  rotateY.m[2][2] = std::cos(rotate.y);
+  rotateY.m[2][3] = 0.0f;
+  rotateY.m[3][0] = 0.0f;
+  rotateY.m[3][1] = 0.0f;
+  rotateY.m[3][2] = 0.0f;
+  rotateY.m[3][3] = 1.0f;
+
+  Matrix4x4 rotateZ;
+  rotateZ.m[0][0] = std::cos(rotate.z);
+  rotateZ.m[0][1] = std::sin(rotate.z);
+  rotateZ.m[0][2] = 0.0f;
+  rotateZ.m[0][3] = 0.0f;
+  rotateZ.m[1][0] = -std::sin(rotate.z);
+  rotateZ.m[1][1] = std::cos(rotate.z);
+  rotateZ.m[1][2] = 0.0f;
+  rotateZ.m[1][3] = 0.0f;
+  rotateZ.m[2][0] = 0.0f;
+  rotateZ.m[2][1] = 0.0f;
+  rotateZ.m[2][2] = 1.0f;
+  rotateZ.m[2][3] = 0.0f;
+  rotateZ.m[3][0] = 0.0f;
+  rotateZ.m[3][1] = 0.0f;
+  rotateZ.m[3][2] = 0.0f;
+  rotateZ.m[3][3] = 1.0f;
+
+  Matrix4x4 rotateMatrix = Multiply(rotateX, Multiply(rotateY, rotateZ));
+
+  Matrix4x4 translateMatrix;
+  translateMatrix.m[0][0] = 1.0f;
+  translateMatrix.m[0][1] = 0.0f;
+  translateMatrix.m[0][2] = 0.0f;
+  translateMatrix.m[0][3] = 0.0f;
+  translateMatrix.m[1][0] = 0.0f;
+  translateMatrix.m[1][1] = 1.0f;
+  translateMatrix.m[1][2] = 0.0f;
+  translateMatrix.m[1][3] = 0.0f;
+  translateMatrix.m[2][0] = 0.0f;
+  translateMatrix.m[2][1] = 0.0f;
+  translateMatrix.m[2][2] = 1.0f;
+  translateMatrix.m[2][3] = 0.0f;
+  translateMatrix.m[3][0] = translate.x;
+  translateMatrix.m[3][1] = translate.y;
+  translateMatrix.m[3][2] = translate.z;
+  translateMatrix.m[3][3] = 1.0f;
+
+  Matrix4x4 worldMatrix;
+  worldMatrix = Multiply(scaleMatrix, Multiply(rotateMatrix, translateMatrix));
+
+  return worldMatrix;
+}
+
+// 描画
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 &matrix) {
+
+  for (int row = 0; row < 4; ++row) {
+    for (int column = 0; column < 4; ++column) {
+      Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight,
+                           "%6.02f", matrix.m[row][column]);
+    }
+  }
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+  // ライブラリの初期化
+  Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+  // キー入力結果を受け取る箱
+  char keys[256] = {0};
+  char preKeys[256] = {0};
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+  Vector3 scale{1.2f, 0.79f, -2.1f};
+  Vector3 rotate{0.4f, 1.43f, -0.8f};
+  Vector3 translate{2.7f, -4.15f, 1.57f};
+  Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+  // ウィンドウの×ボタンが押されるまでループ
+  while (Novice::ProcessMessage() == 0) {
+    // フレームの開始
+    Novice::BeginFrame();
 
-		///
-		/// ↓更新処理ここから
-		///
+    // キー入力を受け取る
+    memcpy(preKeys, keys, 256);
+    Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑更新処理ここまで
-		///
+    ///
+    /// ↓更新処理ここから
+    ///
 
-		///
-		/// ↓描画処理ここから
-		///
+    ///
+    /// ↑更新処理ここまで
+    ///
 
-		///
-		/// ↑描画処理ここまで
-		///
+    ///
+    /// ↓描画処理ここから
+    ///
 
-		// フレームの終了
-		Novice::EndFrame();
+    MatrixScreenPrintf(0, 0, worldMatrix);
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+    ///
+    /// ↑描画処理ここまで
+    ///
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+    // フレームの終了
+    Novice::EndFrame();
+
+    // ESCキーが押されたらループを抜ける
+    if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+      break;
+    }
+  }
+
+  // ライブラリの終了
+  Novice::Finalize();
+  return 0;
 }
