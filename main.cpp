3c8fbf2e24d27dbb9781ffed231f910d17f809d5
Yuya-Kojima <k024g0104@g.neec.ac.jp>
2025-04-18 12:12:30 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,52 +1,159 @@
 #include <Novice.h>
+#include <cmath>
 
 const char kWindowTitle[] = "LC2B_09_コジマユウヤ";
+
+struct Matrix4x4 {
+  float m[4][4];
+};
+
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio,
+                                   float nearClip, float farClip) {
+  Matrix4x4 matrix;
+
+  matrix.m[0][0] =
+      1.0f / aspectRatio * (std::cos(fovY / 2.0f) / std::sin(fovY / 2.0f));
+  matrix.m[0][1] = 0.0f;
+  matrix.m[0][2] = 0.0f;
+  matrix.m[0][3] = 0.0f;
+  matrix.m[1][0] = 0.0f;
+  matrix.m[1][1] = std::cos(fovY / 2.0f) / std::sin(fovY / 2.0f);
+  matrix.m[1][2] = 0.0f;
+  matrix.m[1][3] = 0.0f;
+  matrix.m[2][0] = 0.0f;
+  matrix.m[2][1] = 0.0f;
+  matrix.m[2][2] = farClip / (farClip - nearClip);
+  matrix.m[2][3] = 1.0f;
+  matrix.m[3][0] = 0.0f;
+  matrix.m[3][1] = 0.0f;
+  matrix.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+  matrix.m[3][3] = 0.0f;
+
+  return matrix;
+}
+
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right,
+                                 float bottom, float nearClip, float farClip) {
+  Matrix4x4 matrix;
+
+  matrix.m[0][0] = 2.0f / (right - left);
+  matrix.m[0][1] = 0.0f;
+  matrix.m[0][2] = 0.0f;
+  matrix.m[0][3] = 0.0f;
+  matrix.m[1][0] = 0.0f;
+  matrix.m[1][1] = 2.0f / (top - bottom);
+  matrix.m[1][2] = 0.0f;
+  matrix.m[1][3] = 0.0f;
+  matrix.m[2][0] = 0.0f;
+  matrix.m[2][1] = 0.0f;
+  matrix.m[2][2] = 1.0f / (farClip / nearClip);
+  matrix.m[2][3] = 0.0f;
+  matrix.m[3][0] = (left + right) / (left - right);
+  matrix.m[3][1] = (top + bottom) / (bottom - top);
+  matrix.m[3][2] = nearClip / (nearClip - farClip);
+  matrix.m[3][3] = 1.0f;
+
+  return matrix;
+}
+
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height,
+                             float minDepth, float maxDepth) {
+
+  Matrix4x4 matrix;
+
+  matrix.m[0][0] = width / 2.0f;
+  matrix.m[0][1] = 0.0f;
+  matrix.m[0][2] = 0.0f;
+  matrix.m[0][3] = 0.0f;
+  matrix.m[1][0] = 0.0f;
+  matrix.m[1][1] = -(height / 2.0f);
+  matrix.m[1][2] = 0.0f;
+  matrix.m[1][3] = 0.0f;
+  matrix.m[2][0] = 0.0f;
+  matrix.m[2][1] = 0.0f;
+  matrix.m[2][2] = maxDepth - minDepth;
+  matrix.m[2][3] = 0.0f;
+  matrix.m[3][0] = left + (width / 2.0f);
+  matrix.m[3][1] = top + (height / 2.0f);
+  matrix.m[3][2] = minDepth;
+  matrix.m[3][3] = 1.0f;
+
+  return matrix;
+}
+
+// 描画
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4 &matrix) {
+
+  for (int row = 0; row < 4; ++row) {
+    for (int column = 0; column < 4; ++column) {
+      Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight,
+                           "%6.02f", matrix.m[row][column]);
+    }
+  }
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+  // ライブラリの初期化
+  Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+  // キー入力結果を受け取る箱
+  char keys[256] = {0};
+  char preKeys[256] = {0};
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+  Matrix4x4 orthographicMatrix =
+      MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+  Matrix4x4 perspectiveFovMatrix =
+      MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+  Matrix4x4 viewportMatrix =
+      MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+  // ウィンドウの×ボタンが押されるまでループ
+  while (Novice::ProcessMessage() == 0) {
+    // フレームの開始
+    Novice::BeginFrame();
 
-		///
-		/// ↓更新処理ここから
-		///
+    // キー入力を受け取る
+    memcpy(preKeys, keys, 256);
+    Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↑更新処理ここまで
-		///
+    ///
+    /// ↓更新処理ここから
+    ///
 
-		///
-		/// ↓描画処理ここから
-		///
+    ///
+    /// ↑更新処理ここまで
+    ///
 
-		///
-		/// ↑描画処理ここまで
-		///
+    ///
+    /// ↓描画処理ここから
+    ///
 
-		// フレームの終了
-		Novice::EndFrame();
+    MatrixScreenPrintf(0, 0, orthographicMatrix);
+    MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix);
+    MatrixScreenPrintf(0, kRowHeight * 5 * 2, viewportMatrix);
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+    ///
+    /// ↑描画処理ここまで
+    ///
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+    // フレームの終了
+    Novice::EndFrame();
+
+    // ESCキーが押されたらループを抜ける
+    if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+      break;
+    }
+  }
+
+  // ライブラリの終了
+  Novice::Finalize();
+  return 0;
 }
